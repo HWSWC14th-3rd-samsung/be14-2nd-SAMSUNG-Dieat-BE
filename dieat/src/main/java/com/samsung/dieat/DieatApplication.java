ab89f389d7c9449be9c90f6cbc8b6e078ba31e71
jakego1 <jakego13129@gmail.com>
2025-03-19 19:38:40 +0900

    "Add .gitkeep to track empty folder"

---
+++ dieat/src/main/java/com/samsung/dieat/DieatApplication.java
@@ -0,0 +1,13 @@
+package com.samsung.dieat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DieatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DieatApplication.class, args);
+    }
+
+}
