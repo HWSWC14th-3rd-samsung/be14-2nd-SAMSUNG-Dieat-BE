65968ce4e4f84311c23bbe451a0fd702562fe1cc
1214alex <jerry20011214@gmail.com>
2025-03-21 09:06:45 +0900

    no message

---
+++ dieat/src/main/java/com/samsung/dieat/member/command/domain/aggregate/vo/RequestRegistUserVO.java
@@ -0,0 +1,18 @@
+package com.samsung.dieat.member.command.domain.aggregate.vo;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class RequestRegistUserVO {
+    private String userName;
+    private String userId;
+    private String userPwd;
+    private String userEmail;
+    private String userTel;
+    private String userGender;
+    private String userBirthday;
+}
