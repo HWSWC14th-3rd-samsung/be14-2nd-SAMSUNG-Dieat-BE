ae41d85d141ab9612d966f3ed78b107e009027d8
1214alex <68007279+1214alex@users.noreply.github.com>
2025-03-22 17:58:57 +0900

    Delete dieat directory

--- dieat/src/main/java/com/samsung/dieat/member/security/JwtAuthenticationProvider.java
+++
@@ -1,5 +0,0 @@
-package com.samsung.dieat.member.security;
-
-public class JwtAuthenticationProvider {
-
-}
--- dieat/src/main/java/com/samsung/dieat/point/command/domain/enums/PType.java
+++
@@ -1,5 +0,0 @@
-package com.samsung.dieat.point.command.domain.enums;
-
-public enum PType {
-    REGIST_USERFOOD, ACCURATE_USERFOOD, REVIEW_USERFOOD, MEAL, DIET, FREE, SUCCESS
-}
