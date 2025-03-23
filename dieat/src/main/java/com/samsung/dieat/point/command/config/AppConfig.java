680b4b955ba2f20cc9c1766cbcbb2516a6e25125
ddxyxxn <roserosie23@naver.com>
2025-03-23 22:52:18 +0900

    feature/point

--- dieat/src/main/java/com/samsung/dieat/point/command/config/AppConfig.java
+++ dieat/src/main/java/com/samsung/dieat/point/command/config/AppConfig.java
@@ -1,9 +1,7 @@
 package com.samsung.dieat.point.command.config;
 
-
-import com.samsung.dieat.point.command.application.dto.PointDTO;
-import com.samsung.dieat.point.command.domain.aggregate.entity.PointEntity;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +10,8 @@ public class AppConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);      /*xx. 강력하게 mapping 해라!! */
+        return modelMapper;
     }
 }
--- dieat/src/main/java/com/samsung/dieat/point/command/domain/aggregate/entity/DailyPointEntity.java
+++ dieat/src/main/java/com/samsung/dieat/point/command/domain/aggregate/entity/DailyPointEntity.java
@@ -1,11 +1,10 @@
 package com.samsung.dieat.point.command.domain.aggregate.entity;
 
+import com.samsung.dieat.member.command.domain.aggregate.entity.UserEntity;
+import com.samsung.dieat.member.command.domain.aggregate.entity.UserInfoEntity;
 import com.samsung.dieat.point.command.domain.enums.PType;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
@@ -14,28 +13,29 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Getter
 @Setter
+@ToString
 @Table(name = "tbl_daily_point")
 public class DailyPointEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "d_point_code")
-    private int d_point_code;
+    @Column(name = "d_point_code")                                              // 포인트 적립 코드
+    private int dPointCode;
 
-    @ManyToOne
-    @JoinColumn(name = "d_point_user_code", nullable = false)
-    private Member member;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "d_point_user_code", nullable = false)                   // 포인트 적립 받는 회원 코드
+    private UserInfoEntity dPointUserCode;
 
-    @Column(name = "d_point_rcv_amt", nullable = false, length = 50000)
-    private int d_point_rcv_amt;
+    @Column(name = "d_point_rcv_amt", nullable = false, length = 10)         // 포인트 적립 양
+    private int dPointRcvAmt = 0;
 
-    @Column(name = "d_point_rcv_dt", nullable = false)
-    private LocalDateTime d_point_rcv_dt;
+    @Column(name = "d_point_rcv_dt", nullable = false)                          // 포인트 적립 일시
+    private LocalDateTime dPointRcvDt = LocalDateTime.now();
 
-    @Enumerated(EnumType.STRING)
+    @Enumerated(EnumType.STRING)                                                // 포인트 적립 대상 구분
 //    @Column(name = "d_point_rcv_type", nullable = false)
-    private PType d_point_rcv_type;
+    private PType dPointRcvType;
 
-    @Column(name = "d_point_rcv_code", nullable = false, length = 1000)
-    private int d_point_rcv_code;
+    @Column(name = "d_point_rcv_code", nullable = false, length = 1000)         // 포인트 적립 대상 코드
+    private int dPointRcvCode;
 }
