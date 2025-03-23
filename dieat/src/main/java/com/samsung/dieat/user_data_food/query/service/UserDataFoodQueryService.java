dc459b068392460e6c9c017899af8a827631f29d
mins00kwon <mskwon0918@gmail.com>
2025-03-23 23:24:24 +0900

    feature: user_data_food 검색 /like/메뉴 이름으로 검색했을 때 띄어쓰기에 상관 없이 검색되도록 수정 + 대충 검색해도 잘 검색되도록 수정

--- dieat/src/main/java/com/samsung/dieat/user_data_food/query/service/UserDataFoodQueryService.java
+++ dieat/src/main/java/com/samsung/dieat/user_data_food/query/service/UserDataFoodQueryService.java
@@ -2,6 +2,7 @@ package com.samsung.dieat.user_data_food.query.service;
 
 import com.samsung.dieat.user_data_food.query.dto.ResponseUdfName;
 import com.samsung.dieat.user_data_food.query.dto.ResponseUserDataFood;
+import java.util.Arrays;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,9 +19,11 @@ public class UserDataFoodQueryService {
         return userDataFoodMapper.findByUdfCode(udfCode);
     }
 
-    public List<ResponseUdfName> getUserDataFoodByLikeName(String likeName) {
-        return userDataFoodMapper.findByLikeName(likeName);
+    public List<ResponseUdfName> getUserDataFoodByLikeName(String keyword) {
+        List<String> keywords = Arrays.asList(keyword.split("\\s+")); // 공백 기준으로 키워드 분리
+        return userDataFoodMapper.findByMultipleKeywords(keywords);
     }
+
 
     public List<ResponseUserDataFood> getUserDataFoodWithNickname(String udfName) {
         return userDataFoodMapper.findByUdfNameWithNickname(udfName);
