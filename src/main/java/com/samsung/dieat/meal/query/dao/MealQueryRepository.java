e6ab727e74bd6d19fa935b17e9a1ac6b8f8cc115
JakeGo <161000881+jakego1@users.noreply.github.com>
2025-03-26 01:41:53 +0900

    Delete src/main directory

--- src/main/java/com/samsung/dieat/meal/query/dao/MealQueryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.samsung.dieat.meal.query.dao;
-
-import com.samsung.dieat.meal.command.domain.aggregate.entity.Meal;
-
-import java.util.List;
-
-public interface MealQueryRepository {
-    List<Meal> findMealsByMealCode(int mealCode);
-}
