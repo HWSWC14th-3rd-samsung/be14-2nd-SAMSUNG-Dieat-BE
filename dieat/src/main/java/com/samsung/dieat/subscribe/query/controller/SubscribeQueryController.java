1c03025b4ad92798ea9748cf1ca2edd818fedf02
mins00kwon <mskwon0918@gmail.com>
2025-04-06 00:08:27 +0900

    feat: 구독 -구독 목록 조회 기능 구현 완료

---
+++ dieat/src/main/java/com/samsung/dieat/subscribe/query/controller/SubscribeQueryController.java
@@ -0,0 +1,43 @@
+package com.samsung.dieat.subscribe.query.controller;
+
+import com.samsung.dieat.security.CustomUserDetails;
+import com.samsung.dieat.security.JwtUtil;
+import com.samsung.dieat.subscribe.query.dto.SubscribedUserVO;
+import com.samsung.dieat.subscribe.query.service.SubscribeQueryService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/subscribes")
+public class SubscribeQueryController {
+
+    private final SubscribeQueryService subscribeQueryService;
+    private final JwtUtil jwtUtil;
+
+    @Autowired
+    public SubscribeQueryController(SubscribeQueryService subscribeQueryService, JwtUtil jwtUtil) {
+        this.subscribeQueryService = subscribeQueryService;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<?> getSubscribedUsers(HttpServletRequest request) {
+        String token = request.getHeader("Authorization").substring(7);
+
+        if (jwtUtil.validateToken(token)) {
+            CustomUserDetails userDetails =
+                    (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            int userCode = userDetails.getUserCode();
+
+            List<SubscribedUserVO> list = subscribeQueryService.getSubscribedUsers(userCode);
+            return ResponseEntity.ok(list);
+        }
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("토큰 인증 실패");
+    }
+}
