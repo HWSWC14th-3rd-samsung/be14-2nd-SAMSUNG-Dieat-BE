ee2c2196566fed506eb3bcb25883cbeb7540fe61
mins00kwon <mskwon0918@gmail.com>
2025-04-06 01:47:05 +0900

    feat: 구독 -구독중인 회원들이 올린 게시물 조회하는 기능 구현 완료

--- dieat/src/main/java/com/samsung/dieat/subscribe/query/controller/SubscribeQueryController.java
+++ dieat/src/main/java/com/samsung/dieat/subscribe/query/controller/SubscribeQueryController.java
@@ -2,7 +2,9 @@ package com.samsung.dieat.subscribe.query.controller;
 
 import com.samsung.dieat.security.CustomUserDetails;
 import com.samsung.dieat.security.JwtUtil;
+import com.samsung.dieat.subscribe.query.dto.SubscribedPostVO;
 import com.samsung.dieat.subscribe.query.dto.SubscribedUserVO;
+import com.samsung.dieat.subscribe.query.service.SubscribePostQueryService;
 import com.samsung.dieat.subscribe.query.service.SubscribeQueryService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,11 +19,15 @@ import java.util.List;
 public class SubscribeQueryController {
 
     private final SubscribeQueryService subscribeQueryService;
+    private final SubscribePostQueryService subscribePostQueryService;
     private final JwtUtil jwtUtil;
 
     @Autowired
-    public SubscribeQueryController(SubscribeQueryService subscribeQueryService, JwtUtil jwtUtil) {
+    public SubscribeQueryController(SubscribeQueryService subscribeQueryService,
+                                    JwtUtil jwtUtil,
+                                    SubscribePostQueryService subscribePostQueryService) {
         this.subscribeQueryService = subscribeQueryService;
+        this.subscribePostQueryService = subscribePostQueryService;
         this.jwtUtil = jwtUtil;
     }
 
@@ -40,4 +46,20 @@ public class SubscribeQueryController {
 
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("토큰 인증 실패");
     }
+    @GetMapping("/posts")
+    public ResponseEntity<?> getSubscribedPosts(HttpServletRequest request) {
+        String token = request.getHeader("Authorization").substring(7);
+
+        if (jwtUtil.validateToken(token)) {
+            CustomUserDetails userDetails =
+                    (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            int userCode = userDetails.getUserCode();
+
+            List<SubscribedPostVO> posts = subscribePostQueryService.getSubscribedPosts(userCode);
+            return ResponseEntity.ok(posts);
+        }
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("토큰 인증 실패");
+    }
+
 }
